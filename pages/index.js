cd576f2200838c3c920e5e2c352e58041cf08096
Gabriel Deggau <gabriel.deggau@premiersoft.net>
2021-01-25 23:40:17 -0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,47 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import db from "../db.json";
+import { Widget } from "../src/components/Widget";
+import QuizBackground from "../src/components/QuizBackground";
+import Footer from "../src/components/Footer";
+import GitHubCorner from "../src/components/GitHubCorner";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const BackgroundImage = styled.div`
+//   background-image: url(${db.bg});
+//   flex: 1;
+//   background-position: center;
+//   background-size: cover;
+// `;
+
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>head</Widget.Header>
+          <Widget.Content>
+            <p>testeee</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Header>head</Widget.Header>
+          <Widget.Content>
+            <p>testeee</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="" />
+    </QuizBackground>
+  );
 }
